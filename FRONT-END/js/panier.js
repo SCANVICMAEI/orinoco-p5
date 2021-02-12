981eea766288c86efc13500ff132bbf4c45093bd
Maei <scanvicmaei@yahoo.fr>
2021-02-12 13:59:32 +0100

    vider panier en entier

--- FRONT-END/js/panier.js
+++ FRONT-END/js/panier.js
@@ -1,53 +1,75 @@
-//PANIER
+// SECESSION PANIER
 
 let container = document.getElementById("container");
 
-let cameras = JSON.parse(localStorage.getItem("panier")) ? JSON.parse(localStorage.getItem("panier")) : []; //je récupère les donner de mon panier que j'appele et je le traduit avec parse
+//Recuperation du panier en localstorage
+let cameras = JSON.parse(localStorage.getItem("panier")) ? JSON.parse(localStorage.getItem("panier")) : []; 
 
-let prixPanier = 0 // je déclare en prix panier total a 0 €
-for (let i = 0; i < cameras.length; i++) {//boucle :je parcour les lignes du panier
- 
+// inistialise le total a
+let prixPanier = 0 
+
+//vider bloc 
+
+//Fonction Boucle elements du panier 
+
+for (let i = 0; i < cameras.length; i++) 
+{
   if (cameras != null) {
-    container.innerHTML += //bloque produit
+    container.innerHTML += 
     ` 
     <tr> 
     <td><img src=${cameras[i].imageUrl} alt="" /></td>
     <td>${cameras[i].name}</td>
     <td>${cameras[i].price / 100} €</td>
     <td>${cameras[i].quantity}</td>
-    <td><a> <i class="fas fa-trash-alt" id="deleteCamera"></i></a></td>
+    <td><a> <i class="fas fa-trash-alt" id="deleteCamera-${i}"></i></a></td>
     <td >${cameras[i].quantity * cameras[i].price / 100} €</td>
     </tr>
     `
+    
   }
 
-  prixPanier += cameras[i].quantity * cameras[i].price / 100; //Calcule prix total panier
-  document.getElementById('prixTotal').textContent =  prixPanier + " € "; //j'affiche mon prix total panier
 
+  //Calcule prix total panier
+  function afficherPrixPanier(){ 
+    prixPanier += cameras[i].quantity * cameras[i].price / 100; 
+  }
+
+  //j'affiche mon prix total panier
+  document.getElementById('prixTotal').textContent =  prixPanier + " € "; 
+  
 }
 
-let supprimer = document.getElementById('deleteCamera') //fonction supprimer 1 quantité
-supprimer.addEventListener('click', function()
-{
-  if (cameras.quantity > 1) {
-    cameras.quantity--;
+
+
+// //fonction supprimer 1 quantité
+// let supprimer = document.getElementById('deleteCamera-'+ i) 
+// supprimer.addEventListener('click', function(){
+//   if (cameras[i].quantity > 1) {
+//     cameras[i].quantity--;
+//   } else {
+//     cameras.splice(i);
+//   }
+//   localStorage.setItem('panier', JSON.stringify(cameras))
+// });
+
+
+
+
+
+
+//fonction supprimer le panier
+let viderPanier = document.getElementById('viderPanier') 
+viderPanier.addEventListener('click', function(){
+  if (cameras == null) {   
   } else {
-   cameras.splice();
-  }
-
-});
-
-
-let viderPanier = document.getElementById('viderPanier') //fonction supprimer tous le panier
-viderPanier.addEventListener('click', function()
-{
-  if (panier != null) {
+    container.remove()
     localStorage.clear();
-  } else {
-   alert('Votre panier est déja vide')
+    window.location.reload(); 
   }
-  
 });
+
+
 
 
 
