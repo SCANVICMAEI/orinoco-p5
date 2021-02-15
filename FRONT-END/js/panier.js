bf7ab34833ee6a52cb09d6de331735fc97aee55b
Maei <scanvicmaei@yahoo.fr>
2021-02-15 11:02:12 +0100

    panier quantité

--- FRONT-END/js/panier.js
+++ FRONT-END/js/panier.js
@@ -3,20 +3,16 @@
 let container = document.getElementById("container");
 
 //Recuperation du panier en localstorage
-let cameras = JSON.parse(localStorage.getItem("panier")) ? JSON.parse(localStorage.getItem("panier")) : []; 
+let cameras = JSON.parse(localStorage.getItem("panier")) ? JSON.parse(localStorage.getItem("panier")) : [];
 
-// inistialise le total a
-let prixPanier = 0 
 
-//vider bloc 
 
-//Fonction Boucle elements du panier 
+// Fonction Boucle elements du panier 
 
-for (let i = 0; i < cameras.length; i++) 
-{
+for (let i = 0; i < cameras.length; i++) {
   if (cameras != null) {
-    container.innerHTML += 
-    ` 
+    container.innerHTML +=
+      ` 
     <tr> 
     <td><img src=${cameras[i].imageUrl} alt="" /></td>
     <td>${cameras[i].name}</td>
@@ -26,57 +22,79 @@ for (let i = 0; i < cameras.length; i++)
     <td >${cameras[i].quantity * cameras[i].price / 100} €</td>
     </tr>
     `
-    
   }
 
+  // supprimer 1 quantité
+  let supprimer = document.getElementById('deleteCamera-'+ i) 
+  supprimer.addEventListener('click', function(){
+    if (cameras[i].quantity > 1) {
+      cameras[i].quantity--;
+    } else {
+      cameras.splice(i);
+    }
+    localStorage.setItem('panier', JSON.stringify(cameras))
+    window.location.reload(); 
+  });
 
-  //Calcule prix total panier
-  function afficherPrixPanier(){ 
-    prixPanier += cameras[i].quantity * cameras[i].price / 100; 
-  }
-
-  //j'affiche mon prix total panier
-  document.getElementById('prixTotal').textContent =  prixPanier + " € "; 
-  
 }
 
 
-
-// //fonction supprimer 1 quantité
-// let supprimer = document.getElementById('deleteCamera-'+ i) 
-// supprimer.addEventListener('click', function(){
-//   if (cameras[i].quantity > 1) {
-//     cameras[i].quantity--;
-//   } else {
-//     cameras.splice(i);
-//   }
-//   localStorage.setItem('panier', JSON.stringify(cameras))
-// });
-
-
-
-
-
-
-//fonction supprimer le panier
+//fonction  supprimer le panier
 let viderPanier = document.getElementById('viderPanier') 
 viderPanier.addEventListener('click', function(){
   if (cameras == null) {   
   } else {
-    container.remove()
+    container.remove();
     localStorage.clear();
     window.location.reload(); 
   }
 });
 
+// inistialise le total a
+let prixPanier = 0
 
+ //j'affiche mon prix total panier
+ document.getElementById('prixTotal').textContent =  prixPanier + " € "; 
 
-
+ //Calcule prix total panier
+ function afficherPrixPanier(){ 
+  prixPanier += cameras[i].quantity * cameras[i].price / 100; 
+}
 
 
 
 //FORMULAIRE
 
 
+// let envoiFormulaire = document.getElementById("envoiFormulaire");
+// envoiFormulaire.addEventListener('click',function(){
 
 
+// })
+
+
+// fetch('http://localhost:3000/api/cameras/order',{
+//   method: 'POST',
+//   headers : {
+//     'content-type': 'application/json'
+//   },
+//   body : JSON.stringify(valide)
+// })  
+
+// // .then(response => response.json())   
+// //     .then(response => {
+// //       localStorage.clear();
+
+// let contact = {
+//   nom = document.getElementById("nom").value,
+//   prenom = document.getElementById('prenom').value,
+//   adresse = document.getElementById('adresse').value,
+//   ville = document.getElementById("ville").value,
+//   email = document.getElementById("email").value,
+// };
+
+// let products = cameras.id;
+// formulaireClient = JSON.stringify({
+//   contact,
+//   products,
+// });
